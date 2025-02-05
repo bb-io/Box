a6e79a50c6c76c5fe07016b8c638767ce6b10d08
Mihaly Hobor <mihaly.hobor@blackbird.io>
2025-02-05 15:12:22 +0100

    Add tests to solution

---
+++ Tests.Apps.Box/Base/FileManager.cs
@@ -0,0 +1,43 @@
+﻿using Blackbird.Applications.Sdk.Common.Files;
+using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Apps.Box.Base;
+public class FileManager(string folderLocation) : IFileManagementClient
+{
+    public Task<Stream> DownloadAsync(FileReference reference)
+    {
+        var outputPath = Path.Combine(folderLocation, @$"Output\{reference.Name}");
+        if (File.Exists(outputPath))
+        {
+            var bytes = File.ReadAllBytes(outputPath);
+            var stream = new MemoryStream(bytes);
+            return Task.FromResult((Stream)stream);
+        }
+
+        var inputPath = Path.Combine(folderLocation, @$"Input\{reference.Name}");
+        if (File.Exists(inputPath))
+        {
+            var bytes = File.ReadAllBytes(inputPath);
+            var stream = new MemoryStream(bytes);
+            return Task.FromResult((Stream)stream);
+        }
+        throw new FileNotFoundException($"File '{reference.Name}' not found in either Input or Output folder.");
+    }
+
+    public Task<FileReference> UploadAsync(Stream stream, string contentType, string fileName)
+    {
+        var path = Path.Combine(folderLocation, @$"Output\{fileName}");
+        new FileInfo(path).Directory.Create();
+        using (var fileStream = File.Create(path))
+        {
+            stream.CopyTo(fileStream);
+        }
+
+        return Task.FromResult(new FileReference() { Name = fileName });
+    }
+}
---
+++ Tests.Apps.Box/Base/TestBase.cs
@@ -0,0 +1,42 @@
+﻿using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Apps.Box.Base;
+public class TestBase
+{
+    public IEnumerable<AuthenticationCredentialsProvider> Creds { get; set; }
+
+    public InvocationContext InvocationContext { get; set; }
+
+    public FileManager FileManager { get; set; }
+
+    public TestBase()
+    {
+        var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        Creds = config.GetSection("ConnectionDefinition").GetChildren().Select(x => new AuthenticationCredentialsProvider(x.Key, x.Value)).ToList();
+        var folderLocation = config.GetSection("TestFolder").Value;
+
+        InvocationContext = new InvocationContext
+        {
+            AuthenticationCredentialsProviders = Creds,
+            UriInfo = new UriInfo() { AuthorizationCodeRedirectUri= new Uri("https://localhost")}
+        };
+
+        FileManager = new FileManager(folderLocation);
+    }
+
+    public ILogger<T> CreateLogger<T>()
+    {
+        using var loggerFactory = LoggerFactory.Create(loggingBuilder => loggingBuilder
+        .SetMinimumLevel(LogLevel.Trace));
+
+        return loggerFactory.CreateLogger<T>();
+    }
+}
