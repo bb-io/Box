7a35e2602aad2a082e61e239ba6c7b613eca1cd1
vitaliy-bezugly <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2026-08-18 12:42:42 +0300

    Update ApplicationConstants.cs

--- Apps.Box/ApplicationConstants.cs
+++ Apps.Box/ApplicationConstants.cs
@@ -4,5 +4,5 @@ public static class ApplicationConstants
 {
     public const string ClientId = "#{BOX_CLIENT_ID}#";
     public const string ClientSecret = "#{BOX_SECRET}#";
-    public const string Scope = "#{BOX_SCOPE}#";
+    public const string Scope = "root_readwrite manage_webhook";
 }
